e8ead26c3e5fe06958f7a420ad8d6971496a231f
Aman Prajapati <25114cn366@glbitm.ac.in>
2026-06-15 15:48:35 +0530

    Add files via upload

---
+++ Day-15/ques_60.cpp
@@ -0,0 +1,36 @@
+//Write a program to Move zeroes to end. 
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+
+    cout<<"Enter the size of the array: ";
+    cin>> n;
+
+    int arr[n];
+
+    cout<<"Enter the elements of the array: ";
+    for (int i=0; i<n; i++) {
+        cin >> arr[i];
+    }
+
+    int index = 0;
+    for (int i=0; i<n; i++) {
+        if (arr[i] != 0) {
+            arr[index] = arr[i];
+            index++;
+        }
+    }
+    while (index < n) {
+        arr[index] = 0;
+        index++;
+    }
+
+    cout<< "Array after moving zeroes to the end: ";
+    for (int i=0; i<n; i++) {
+        cout<<arr[i]<< " ";
+    }
+
+    return 0;
+}
