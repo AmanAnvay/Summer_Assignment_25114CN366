31899ccb9fb5e13821d889ea3e4ce07ae0d0b73b
Aman Prajapati <25114cn366@glbitm.ac.in>
2026-06-23 17:18:13 +0530

    Add files via upload

---
+++ Day-23/ques_92.cpp
@@ -0,0 +1,33 @@
+/* Write a program to Find maximum occurring 
+character. 
+*/
+#include<iostream>
+#include<unordered_map>
+using namespace std;
+
+int main() {
+    string str;
+    cout<<"Enter a string: ";
+    getline(cin, str);
+
+    unordered_map<char,int>freq;
+
+    for(char ch : str) {
+        freq[ch]++;
+    }
+
+    char maxChar;
+    int maxFreq = 0;
+
+    for(auto pair : freq){
+        if(pair.second > maxFreq){
+            maxFreq = pair.second;
+            maxChar = pair.first;
+        }
+    }
+
+    cout<<"Maximum occurring character: "<<maxChar<<endl;
+    cout<<"Frequency: "<<maxFreq<<endl;
+
+    return 0;
+}
